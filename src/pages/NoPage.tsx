5f0a10d5cc0c6aadd0055996e3633770ad069a35
Paige Maute <paige.maute@gmail.com>
2024-05-11 20:25:13 +0000

    update content and run prettier

--- src/pages/NoPage.tsx
+++ src/pages/NoPage.tsx
@@ -1,6 +1,6 @@
-import { Typography } from "@mui/material"
-import { FC } from "react"
+import { Typography } from "@mui/material";
+import { FC } from "react";
 
 export const NoPage: FC = () => {
-  return <Typography variant="h1">Sorry.</Typography>
-}
+  return <Typography variant="h1">Sorry.</Typography>;
+};
