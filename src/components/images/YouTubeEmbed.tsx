d609976de4f4fb7d945005301f31752268a7e7a1
Paige Maute <paige.maute@gmail.com>
2024-05-17 23:40:31 +0000

    update embeds

--- src/components/images/YouTubeEmbed.tsx
+++ src/components/images/YouTubeEmbed.tsx
@@ -8,10 +8,10 @@ interface props {
 export const YoutubeEmbed: FC<props> = (props) => {
   const { embedId } = props
   return (
-    <Card sx={{p:1}}>
+    <Card sx={{ p: 1, width: "100%", height: "auto", aspectRatio: "16 / 9" }}>
       <iframe
-        width="853"
-        height="480"
+        width="100%"
+        height="100%"
         src={`https://www.youtube.com/embed/${embedId}`}
         allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
         allowFullScreen
--- src/pages/paige/PortalGun.tsx
+++ src/pages/paige/PortalGun.tsx
@@ -1,4 +1,4 @@
-import { Box, Typography } from "@mui/material"
+import { Box, Card, Grid, Typography } from "@mui/material"
 import { FC } from "react"
 import schematic from "../../assets/paige/portal/PGSchematic.webp"
 import { YoutubeEmbed } from "../../components/images/YouTubeEmbed"
@@ -10,8 +10,16 @@ export const PortalGun: FC = () => {
       <Typography variant="h1" fontWeight={700} mb={2}>
         Aperture Portal Gun Prop
       </Typography>
-      <YoutubeEmbed embedId="LbGg5cHe4nM" />
-      <img src={schematic} />
+      <Grid container spacing={2}>
+        <Grid item xs={12} lg={6}>
+          <YoutubeEmbed embedId="LbGg5cHe4nM" />
+        </Grid>
+        <Grid item>
+          <Card sx={{ p: 1 }}>
+            <img src={schematic} />
+          </Card>
+        </Grid>
+      </Grid>
     </Box>
   )
 }
