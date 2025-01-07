0adb3cf996c236777f51aefb588db327b34f28fc
Paige Maute <paige.maute@gmail.com>
2025-01-07 13:54:10 -0500

    conversion to next.js: main page works

--- src/app/components/layouts/MainLayout.tsx
+++ src/app/components/layouts/MainLayout.tsx
@@ -1,14 +1,29 @@
-import { Box, Link, Typography } from "@mui/material"
+import {
+  Box,
+  Container,
+  Link,
+  styled,
+  Typography,
+  useTheme,
+} from "@mui/material"
 import { Blob } from "../images/Blob"
 
-export const MainLayout = () => {
+const MainLayoutRoot = styled("div")(({ theme }) => ({
+  backgroundColor: theme.palette.background.paper,
+  height: "100vh",
+  width: "100vw",
+  position: "fixed",
+}))
+
+export function MainLayout({ children }: { children: React.ReactNode }) {
+  const theme = useTheme()
   return (
-    <>
+    <MainLayoutRoot>
       <Link href="/" underline="none">
         <Typography
           variant="h2"
           fontWeight={900}
-          color="var(--foreground)" // 2nd
+          color={theme.palette.text.secondary}
           sx={{
             ml: "3vw",
             mt: "2vh",
@@ -31,7 +46,7 @@ export const MainLayout = () => {
           zIndex: -1,
         }}
       >
-        <Blob fill="var(--background)" />
+        <Blob fill={theme.palette.background.default} />
       </Box>
       <Box
         aria-hidden
@@ -52,8 +67,25 @@ export const MainLayout = () => {
           },
         }}
       >
-        <Blob fill="var(--background)" />
+        <Blob fill={theme.palette.background.default} />
       </Box>
-    </>
+      <Container sx={{ display: "flex", justifyContent: "center" }}>
+        <Box
+          sx={{
+            mask: "linear-gradient(to top, rgba(0,0,0, 1) 0, rgba(0,0,0, 1) 97%, rgba(0,0,0, 0) 100%, rgba(0,0,0, 0) 20%);",
+            overflow: "auto",
+            maxHeight: "90vh",
+            position: "relative",
+            width: "85vw",
+            mt: "10vh",
+            paddingBottom: 10,
+            "::-webkit-scrollbar": { display: "none" },
+            scrollbarWidth: "none",
+          }}
+        >
+          {children}
+        </Box>
+      </Container>
+    </MainLayoutRoot>
   )
 }
